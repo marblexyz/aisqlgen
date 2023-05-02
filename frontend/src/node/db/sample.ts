62136f3199d7f41bc6754a87a59c491689a85a82
John Suh <johnsuh94@gmail.com>
2023-05-02 13:07:04 -0400

    allow .env for db connection

--- frontend/src/node/db/sample.ts
+++ frontend/src/node/db/sample.ts
@@ -1,9 +1,13 @@
 // TODO: Change this to use with a database hosted somewhere, like railway.
 export const SAMPLE_PG_DB_CONFIG = {
-  host: "localhost",
-  port: 5434,
-  database: "rizz_local",
-  user: "postgres",
-  password: "postgres",
-  useSSL: true,
+  host: process.env.PGHOST !== undefined ? process.env.PGHOST : "localhost",
+  port: process.env.PGPORT !== undefined ? Number(process.env.PGPORT) : 5434,
+  database:
+    process.env.PGDATABASE !== undefined
+      ? process.env.PGDATABASE
+      : "rizz_local",
+  user: process.env.PGUSER !== undefined ? process.env.PGUSER : "postgres",
+  password:
+    process.env.PGPASSWPRD !== undefined ? process.env.PGPASSWPRD : "postgres",
+  useSSL: process.env.PGUSESSL === undefined || process.env.PGUSESSL === "true",
 };
