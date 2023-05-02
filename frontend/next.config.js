25e8b97f667282dfb25dc8797eb63f6cbd9bee37
Theodor Marcu <marcu.theodor@gmail.com>
2023-05-02 10:57:59 -0400

    fix webpack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   // Enable https://www.npmjs.com/package/@dqbd/tiktoken
-  asyncWebAssembly: true,
-  layers: true,
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
