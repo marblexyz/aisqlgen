94622d2ddc51aeaacce9484f2b8bd2970e50b3c5
Theodor Marcu <marcu.theodor@gmail.com>
2023-05-02 17:22:29 -0400

    add datasource slice

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { hydrateDatasourceConnector } from "@/redux/slices/datasource/datasourceSlice";
 import { hydrateQueryHistory } from "@/redux/slices/queryHistory/queryHistorySlice";
-import store, { loadFromlocalStorage } from "@/redux/store";
-import { QueryMakerLocalStorageState } from "@/types/redux/state";
+import store, {
+  loadDatasourceMapFromLocalStorage,
+  loadQueryHistoryFromLocalStorage,
+} from "@/redux/store";
+import {
+  DatasourceLocalStorageState,
+  QueryMakerLocalStorageState,
+} from "@/types/redux/state";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
@@ -19,10 +26,18 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   useEffect(() => {
     const hydrateReduxOnLoad = async () => {
-      const preloadedState: QueryMakerLocalStorageState =
-        await loadFromlocalStorage();
-      if (preloadedState.queryHistory !== undefined) {
-        store.dispatch(hydrateQueryHistory(preloadedState.queryHistory));
+      const preloadedHistoryState: QueryMakerLocalStorageState =
+        await loadQueryHistoryFromLocalStorage();
+      if (preloadedHistoryState.queryHistory !== undefined) {
+        store.dispatch(hydrateQueryHistory(preloadedHistoryState.queryHistory));
+      }
+      const preloadedDatasourceState: DatasourceLocalStorageState =
+        await loadDatasourceMapFromLocalStorage();
+
+      if (preloadedDatasourceState.datasourceMap !== undefined) {
+        store.dispatch(
+          hydrateDatasourceConnector(preloadedDatasourceState.datasourceMap)
+        );
       }
     };
     void hydrateReduxOnLoad();
---
+++ frontend/src/redux/slices/datasource/datasourceSlice.ts
@@ -0,0 +1,40 @@
+import {
+  DatasourceConfigType,
+  DatasourceMap,
+  DatasourceMapState,
+  DatasourceType,
+} from "@/types/redux/slices/datasourceConnector";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState: DatasourceMapState = {
+  datasourceMap: {},
+};
+
+export const datasourceConnector = createSlice({
+  name: "datasourceConnectorSlice",
+  initialState,
+  reducers: {
+    hydrateDatasourceConnector: (
+      state,
+      action: PayloadAction<DatasourceMap>
+    ) => {
+      state.datasourceMap = action.payload;
+    },
+    addDataSource: (
+      state,
+      action: PayloadAction<{
+        datasourceId: string;
+        datasourceType: DatasourceType;
+        datasourceConfig: DatasourceConfigType;
+      }>
+    ) => {
+      state.datasourceMap[action.payload.datasourceId] = {
+        type: action.payload.datasourceType,
+        config: action.payload.datasourceConfig,
+      };
+    },
+  },
+});
+
+export const { hydrateDatasourceConnector, addDataSource } =
+  datasourceConnector.actions;
