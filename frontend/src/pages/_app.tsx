b2c7af03058ee30ca8d854c8bc0f868186eacfa3
Theodor Marcu <marcu.theodor@gmail.com>
2023-05-02 16:48:56 -0400

    prevent ssr error

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { QueryMakerLocalStorageState } from "@/types/redux/state";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 
 const theme = extendTheme({
@@ -17,13 +17,17 @@ const theme = extendTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  void loadFromlocalStorage().then(
-    (preloadedState: QueryMakerLocalStorageState) => {
+  useEffect(() => {
+    const hydrateReduxOnLoad = async () => {
+      const preloadedState: QueryMakerLocalStorageState =
+        await loadFromlocalStorage();
       if (preloadedState.queryHistory !== undefined) {
         store.dispatch(hydrateQueryHistory(preloadedState.queryHistory));
       }
-    }
-  );
+    };
+    void hydrateReduxOnLoad();
+  }, []);
+
   return (
     <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>
